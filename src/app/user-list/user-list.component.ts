cca5433e6a7845b26ffc34a27803d407436f3f0b
javaproject2022 <java.project.with.friends@gmail.com>
2023-01-18 00:39:18 +0330

    Update Typescript Component to reference Bootstrap HTML table

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -3,7 +3,7 @@ import { UserInfo } from './user-info';
 
 @Component({
   selector: 'app-user-list',
-  templateUrl: './user-list.component.html',
+  templateUrl: './user-list-bootstrap.component.html',
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
