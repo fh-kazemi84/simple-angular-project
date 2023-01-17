ff2d8950496f0ac0c229549dfb909811c1e75cf5
javaproject2022 <java.project.with.friends@gmail.com>
2023-01-17 23:30:01 +0330

    create an array of Objects

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserInfo } from './user-info';
 
 @Component({
   selector: 'app-user-list',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
+
+  //create an array of Objects
+  userInfoList: UserInfo[] = [
+    new UserInfo("Jane","Limp","jane_limp@gmail.com","JaneLimp"),
+    new UserInfo("Simohn","Beg","simohn_beg@gmail.com","SimBeg"),
+    new UserInfo("Nick","kafman","nick_kafman@gmail.com","Nicki"),
+    new UserInfo("Mari","Doe","mari_doe@gmail.com","MariDoe")
+  ];
+
 
   constructor() { }
 
