a744bb189cef0a4a06652ac29805cb5412c0b7d3
javaproject2022 <java.project.with.friends@gmail.com>
2023-01-17 22:35:28 +0330

    edit component -ts and -html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'simple-angular-project';
+  title = 'Angular Project is ';
+
+  firstname = 'Fatimah';
+  lastname = 'Kazemi';
 }
