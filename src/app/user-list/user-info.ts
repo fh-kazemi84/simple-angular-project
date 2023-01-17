05d6ce94a85c8fbe4c849b0718275fa50a17149b
javaproject2022 <java.project.with.friends@gmail.com>
2023-01-17 23:11:24 +0330

    Edit UserInfo class

--- src/app/user-list/user-info.ts
+++ src/app/user-list/user-info.ts
@@ -1,2 +1,9 @@
 export class UserInfo {
+
+    constructor(public firstname: string,
+                public lastname: string,
+                public email: string,
+                public username: string){
+                    
+                }
 }
