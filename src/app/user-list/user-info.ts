8500f9d4bd90aa16ec8ccddcc1244e318622d66b
javaproject2022 <java.project.with.friends@gmail.com>
2023-01-18 00:47:12 +0330

    Add a property ro UserInfo class

--- src/app/user-list/user-info.ts
+++ src/app/user-list/user-info.ts
@@ -3,7 +3,8 @@ export class UserInfo {
     constructor(public firstname: string,
                 public lastname: string,
                 public email: string,
-                public username: string){
+                public username: string,
+                public userId: number){
                     
                 }
 }
